cd9024c4242a25e50b6950f85d6c230e5ab5f538
MOHIT <mohitjyouti2003@gmail.com>
2024-04-25 15:48:20 +0530

    add db

--- init/data.js
+++ init/data.js
@@ -3,12 +3,83 @@ const sampleData=[
        title:"C Programming Notes" ,
        description:"The C programming language is a general-purpose, operating system-agnostic, and procedural language that supports structured programming and provides low-level access to the system memory.",
         image:{
-            url:"https://unsplash.com/photos/a-gold-letter-c-on-a-green-background-85mSSWR2tXI"
+            url:"https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/C_Programming_Language.svg/695px-C_Programming_Language.svg.png"
         },
         pdf:{
             link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
         }
-    }
+    },
+    {
+       title:"Python Notes" ,
+       description:"Python is a computer programming language often used to build websites and software, automate tasks, and analyze data. Python is a general-purpose language, not specialized for any specific problems, and used to create various programmes.",
+        image:{
+            url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3dvurwfqMolKjc2EBcoALj8L1nFWlSGtNF6yeu4ONXw&s"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    {
+       title:"Django Notes" ,
+       description:"Django is a high-level Python web framework that enables rapid development of secure and maintainable websites.",
+        image:{
+            url:"https://media.dev.to/cdn-cgi/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fd0hst9r97gq50860kpq0.png"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    {
+       title:"Git and GitHub Notes" ,
+       description:"GitHub is an online software development platform. It's used for storing, tracking, and collaborating on software projects. It makes it easy for developers to share code files and collaborate with fellow developers on open-source projects.",
+        image:{
+            url:"https://www.webfx.com/wp-content/uploads/2022/08/github-logo.png"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    {
+       title:"Node.js Notes" ,
+       description:"It is used for server-side programming, and primarily deployed for non-blocking, event-driven servers, such as traditional web sites and back-end API services, but was originally designed with real-time, push-based architectures in mind. Every browser has its own version of a JS engine, and node.",
+        image:{
+            url:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/590px-Node.js_logo.svg.png"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    {
+       title:"HTML&CSS Notes" ,
+       description:"HTML is a markup language used to create static web pages and web applications. CSS is a style sheet language responsible for the presentation of documents written in a markup language.",
+        image:{
+            url:"https://miro.medium.com/v2/resize:fit:792/1*lJ32Bl-lHWmNMUSiSq17gQ.png"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    {
+       title:"Javascript Notes" ,
+       description:"JavaScript (JS) is a cross-platform, object-oriented programming language used by developers to make web pages interactive. It allows developers to create dynamically updating content, use animations, pop-up menus, clickable buttons, control multimedia, etc.",
+        image:{
+            url:"https://www.w3schools.com/whatis/img_js.png"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    {
+       title:"Linear Programming Notes" ,
+       description:"linear programming, mathematical modeling technique in which a linear function is maximized or minimized when subjected to various constraints. This technique has been useful for guiding quantitative decisions in business planning, in industrial engineering, and—to a lesser extent—in the social and physical sciences.",
+        image:{
+            url:"https://media.geeksforgeeks.org/wp-content/uploads/20230609122206/LPP-Problem.png"
+        },
+        pdf:{
+            link:"https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/notes/C_Complete_Notes.pdf"
+        }
+    },
+    
 ]
 
 module.exports = { data: sampleData }
